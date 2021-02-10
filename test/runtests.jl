a30f229596a0894aa392849adf389d79933180cd
0x0f0f0f <sudo-woodo3@protonmail.com>
2021-02-10 17:54:56 +0100

    restored all tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,8 +4,8 @@ using Test
 
 include("../src/util.jl")
 
-# include("test_theories.jl")
-# include("test_reductions.jl")
+include("test_theories.jl")
+include("test_reductions.jl")
 include("test_egraphs.jl")
 include("test_ematch.jl")
 include("test_ematch_assertions.jl")
