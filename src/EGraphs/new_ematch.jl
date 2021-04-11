d6ca992d8c88b7f923835617dbc14665b465940e
0x0f0f0f <sudo-woodo3@protonmail.com>
2021-04-11 16:51:36 +0200

    started working on new ematch

--- src/EGraphs/new_ematch.jl
+++ src/EGraphs/new_ematch.jl
@@ -32,3 +32,46 @@ function compile_pat(eclass, p::PatVar, ctx)
         return []
     end
 end
+
+function compile_pat(p::Pattern)
+    ctx = Dict()
+    insns = compile_pat(:start, p, ctx)
+    
+    return vcat(insns, Yield(collect(values(ctx)))), ctx
+end
+
+function interp_unstaged(G, insns, ctx, buf) 
+    if length(insns) == 0
+        return 
+    end
+    insn = insns[1]
+    insns = insns[2:end]
+    if insn isa Bind
+        for enode in G[ctx[insn.eclass]] 
+            if enode.head == insn.enodepat.head && length(enode.args) == length(insn.enodepat.args)
+                for (n,v) in enumerate(insn.enodepat.args)
+                    ctx[v] = enode.args[n]
+                end
+                interp_unstaged(G,  insns, ctx, buf)
+            end
+        end
+    elseif insn isa Yield
+        push!( buf, [ctx[y] for y in insn.yields])
+    elseif insn isa CheckClassEq
+        if ctx[insn.eclass1] == ctx[insn.eclass2]
+            interp_unstaged(G, insns, ctx, buf)
+        end
+    end
+end
+
+function interp_unstaged(g::EGraph, program, id)
+    buf = []
+    ctx = Dict{Symbol,Int64}([:start => id])
+    interp_unstaged(g::EGraph, program, ctx, buf)
+    return buf
+end
+
+function ematch(g::EGraph, p::Pattern, id::Int64)
+    program, _ = compile_pat(p)
+    interp_unstaged(g, program, id)
+end
