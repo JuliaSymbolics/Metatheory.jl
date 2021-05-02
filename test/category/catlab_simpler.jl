c22b030d2224ea42d54647ddc410fc23a6c0c6e2
Chen Zhao <zhaochen17@mails.ucas.ac.cn>
2021-05-02 22:57:51 +0800

    trying to fix wrong matching

--- test/category/catlab_simpler.jl
+++ test/category/catlab_simpler.jl
@@ -5,100 +5,85 @@ using Catlab.Syntax
 using Metatheory, Metatheory.EGraphs
 @metatheory_init ()
 
-# WE HAVE TO REDEFINE THE SYNTAX TO AVOID ASSOCIATIVITY AND N-ARY FUNCTIONS
-import Catlab.Theories: id, compose, otimes, ⋅, braid, σ, ⊗, Ob, Hom
-@syntax SMC{ObExpr,HomExpr} SymmetricMonoidalCategory begin
-end
-
-A, B, C, D = Ob(SMC, :A, :B, :C, :D)
-X, Y, Z = Ob(SMC, :X, :Y, :Z)
-
-f = Hom(:f, A, B)
-g = Hom(:g, B, C)
-h = Hom(:h, C, D)
-
-
-
+# GATExpr => normal Expr in MT
 function gat_to_expr(ex::ObExpr{:generator})
     @assert length(ex.args) == 1
     return ex.args[1]
 end
-
 function gat_to_expr(ex::ObExpr{H}) where {H}
     return Expr(:call, head(ex), map(gat_to_expr, ex.args)...)
 end
-
 function gat_to_expr(ex::HomExpr{H}) where {H}
     @assert length(ex.type_args) == 2
     expr = Expr(:call, head(ex), map(gat_to_expr, ex.args)...)
     type_ex = Expr(:call, :Hom, map(gat_to_expr, ex.type_args)...)
     return Expr(:call, :~, expr, type_ex)
 end
-
 function gat_to_expr(ex::HomExpr{:generator})
     f = ex.args[1]
     type_ex = Expr(:call, :Hom, map(gat_to_expr, ex.type_args)...)
     return Expr(:call, :~, f, type_ex)
 end
 
-
-gat_to_expr(x) = x
-
-gat_to_expr(id(Z)) == :(id(Z)~(Z→Z)) 
-
-gat_to_expr(id(Z) ⋅ f)
-
-gat_to_expr(id(A ⊗ B))
-
-gat_to_expr(id(A) ⊗ id(B))
-
-gat_to_expr(compose(compose(f, g), h))
-
-gat_to_expr(f)
-
-tt = SMC.theory() |> theory
-
-
-# base case
-function get_concrete_type_expr(theory, x::Symbol, axiom, loc_ctx = Dict{Code, Code}())
-    ctx = axiom.context
-    t = ctx[x]
-    t === :Ob && (t = x)
-    loc_ctx[x] = t
-    return t
-end
-
 const Code = Union{Symbol, Expr}
 
-function get_concrete_type_expr(theory, x::Expr, axiom, loc_ctx = Dict{Code, Code}())
-    #local context
-    ctx = axiom.context
-    # loc_ctx = Dict{Code, Code}()
+
+# infer type of morphisms and objects
+# a morphism f: A → B will be typed as f ~ Hom(A, B)
+# an object A will be typed as Ob(A)
+function get_concrete_type_expr(theory, x::Symbol, ctx, type_tags = Dict{Code, Code}())
+    t = ctx[x]
+    t === :Ob && (t = Expr(:call, :Ob, x))
+    type_tags[x] = t
+    return t
+end
+function get_concrete_type_expr(theory, x::Expr, ctx, type_tags = Dict{Code, Code}())
     @assert x.head == :call
     f = x.args[1]
     rest = x.args[2:end]
     # recursion case - inductive step (?)
     for a in rest
-        t = get_concrete_type_expr(theory, a, axiom, loc_ctx)
-        loc_ctx[a] = t
+        t = get_concrete_type_expr(theory, a, ctx, type_tags)
+        type_tags[a] = t
         # println("$a ~ $t")
     end
     # get the corresponding TermConstructor from theory.terms
     # for each arg in `rest`, instantiate the term.params with term.context
     # instantiate term.typ
 
-    loc_ctx[x] = gat_type_inference(theory, f, [loc_ctx[a] for a in rest])
-    # println("$x ~ $(loc_ctx[x])")
-    return loc_ctx[x]
+    type_tags[x] = gat_type_inference(theory, f, [type_tags[a] for a in rest])
+    # println("$x ~ $(type_tags[x])")
+    return type_tags[x]
+end
+
+function is_context_match(t, head, args)
+    t.name !== head && return false
+    n = length(t.params)
+    n != length(args) && return false
+    for i = 1:n
+        if t.context[t.params[i]] === :Ob 
+            if (args[i] isa Symbol) || args[i].args[1] !== :Ob
+                return false
+            end
+        else
+            if !(args[i] isa Symbol) && args[i].args[1] === :Ob
+                return false
+            end
+        end
+    end
+    return true
 end
 
 function gat_type_inference(theory, head, args)
     for t in theory.terms
-        t.name === head && return gat_type_inference(t, head, args)
+        if is_context_match(t, head, args)
+            @show t, head, args
+            return gat_type_inference(t, head, args)
     end
-    @error "can not find $head in the theory"
 end
-
+    @show theory, head, args
+    @error "can not find $(Expr(:call, head, args...)) in the theory"
+end
 function gat_type_inference(t::GAT.TermConstructor, head, args)
     @assert length(t.params) == length(args) && t.name === head
     bindings = Dict()
@@ -111,13 +96,12 @@ function gat_type_inference(t::GAT.TermConstructor, head, args)
     # @show bindings
     r = GAT.replace_types(bindings, t)
     if r.typ == :Ob 
-        return Expr(:call, head, args...)
+        return Expr(:call, :Ob, Expr(:call, head, args...))
     else 
         return r.typ
     end
 end
 function update_bindings!(bindings, template::Expr, target::Expr)
-    @assert length(template.args) == length(target.args)
     for i = 1:length(template.args)
         update_bindings!(bindings, template.args[i], target.args[i])
     end
@@ -150,18 +134,24 @@ function tag_expr(x::Symbol, axiom, theory)
 end
 
 
+import Catlab.Theories: id, compose, otimes, ⋅, braid, σ, ⊗, Ob, Hom
+@syntax SMC{ObExpr,HomExpr} SymmetricMonoidalCategory begin
+end
 
+A, B, C, D = Ob(SMC, :A, :B, :C, :D)
+X, Y, Z = Ob(SMC, :X, :Y, :Z)
 
-ax = tt.axioms[3]
-get_concrete_type_expr(tt, ax.left, ax)
-
-tag_expr(:(id(otimes(A,A))), ax, tt) == gat_to_expr(id(otimes(A,A)))
+tt = theory(SymmetricMonoidalCategory)
+ax = tt.axioms[10]
+get_concrete_type_expr(tt, ax.left, ax.context)
+ax = tt.axioms[4]
+get_concrete_type_expr(tt, ax.left, ax.context)
 
 function axiom_to_rule(theory, axiom)
     lhs = tag_expr(axiom.left, axiom, tt) |> Pattern
     rhs = tag_expr(axiom.right, axiom, tt) |> Pattern
 
-    println("$lhs == $rhs")
+    # println("$lhs == $rhs")
     op = axiom.name
     @assert op == :(==)
     EqualityRule(lhs, rhs)
