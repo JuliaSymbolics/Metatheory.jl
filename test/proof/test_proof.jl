d9fa7321fef33dceab555f93413100f01987ffce
0x0f0f0f <sudo-woodo3@protonmail.com>
2021-05-11 22:21:23 +0200

    bfs for searching proof

--- test/proof/test_proof.jl
+++ test/proof/test_proof.jl
@@ -1,11 +1,6 @@
 using Metatheory, Metatheory.EGraphs
 using Test
 
-t = @theory begin 
-    a * b == b * a 
-    a * 2 == a + a
-end
-
 function prove(g::EGraph, t::Vector{<:Rule}, exprs...;
     mod=@__MODULE__, params=SaturationParams())
     @info "Checking equality for " exprs
@@ -36,7 +31,7 @@ function prove(g::EGraph, t::Vector{<:Rule}, exprs...;
     for (id, ec) in g.classes 
         for n in ec 
             println(id => n)
-            println("$n.proof_rules ⩜ $n.proof_src ⩜ $n.proof_trg")
+            println("$(n.proof_rules) ⩜ $(n.proof_src) ⩜ $(n.proof_trg)")
         end 
     end
 
@@ -47,8 +42,8 @@ function prove(g::EGraph, t::Vector{<:Rule}, exprs...;
             eclass = geteclass(g, g.memo[node])
             for nn in eclass
                 if node == nn
-                    println("$n.proof_rules ⩜ $n.proof_src ⩜ $n.proof_trg")
-                    println("$nn.proof_rules ⩜ $nn.proof_src ⩜ $nn.proof_trg")
+                    println("$(node.proof_rules) ⩜ $(node.proof_src) ⩜ $(node.proof_trg)")
+                    println("$(nn.proof_rules) ⩜ $(nn.proof_src) ⩜ $(nn.proof_trg)")
                     println("$node == $nn")
                     nodes[i] = nn
                 end
@@ -57,11 +52,50 @@ function prove(g::EGraph, t::Vector{<:Rule}, exprs...;
     end
     println("========================================")
     for i in 1:n 
-        println(nodes[i].proof)
+        nn = nodes[i]
+        println("$(nn.proof_rules) ⩜ $(nn.proof_src) ⩜ $(nn.proof_trg)")
     end
     println("========================================")
+    proof_bfs(nodes[1], nodes[2])
+end
 
-    # TODO go through each path in the proof. do a BFS?
+mutable struct ProofNode
+    state::ENode
+    cost::Number
+    parent::Union{Nothing, ProofNode}
+end
+
+
+# TODO go through each path in the proof. do a BFS?
+using DataStructures
+function proof_bfs(src::ENode, trg::ENode)
+    root = ProofNode(src, 0, nothing)
+    frontier = ProofNode[]
+    explored = Set{ENode}()
+    push!(frontier, root)
+    while !isempty(frontier)
+        node = popfirst!(frontier)
+        println("exploring $node")
+        push!(explored, node.state)
+        # todo take rules in account
+        for child_enode in node.state.proof_trg
+            child = ProofNode(child_enode, node.cost+1, node)
+            if child_enode ∉ explored && child ∉ frontier
+                # goal test
+                if child_enode == trg
+                    return child 
+                end
+                push!(frontier, child)
+            end
+        end
+    end
+    error("proof not found!")
+end
+
+
+t = @theory begin 
+    a * b == b * a 
+    a * 2 == a + a
 end
 
 
@@ -72,3 +106,6 @@ saturate!(g, t)
 prove(g, t, :(2 * a), :(a + a))  
 
 prove(EGraph(), t, :(2 * x), :(x + x))
+
+include("../logic/prop_logic_theory.jl")
+
