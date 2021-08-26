587859755768308a2cb475f04e21ba313812da00
Alessandro <none>
2021-08-26 20:13:51 +0200

    faster rule application

--- src/EGraphs/saturation/apply.jl
+++ src/EGraphs/saturation/apply.jl
@@ -1,5 +1,3 @@
-const UnionBuf = Vector{Tuple{EClassId, EClassId}}
-
 function (rule::UnequalRule)(g::EGraph, match::Match)
     lc = match.id
     rinst = instantiate(g, match.pat_to_inst, match.sub, rule)
@@ -7,15 +5,16 @@ function (rule::UnequalRule)(g::EGraph, match::Match)
 
     if find(g, lc) == find(g, rc)
         @log "Contradiction!" rule
-        return (Contradiction(), nothing)
+        return Contradiction()
     end
-    return (nothing, nothing)
+    return nothing
 end
 
 function (rule::SymbolicRule)(g::EGraph, match::Match)
     rinst = instantiate(g, match.pat_to_inst, match.sub, rule)
     rc, node = addexpr!(g, rinst)
-    return (nothing, (match.id, rc.id))
+    merge!(g, match.id, rc.id)
+    return nothing
 end
 
 
@@ -24,21 +23,18 @@ function (rule::DynamicRule)(g::EGraph, match::Match)
     actual_params = [instantiate(g, PatVar(v, i), match.sub, rule) for (i, v) in enumerate(rule.patvars)]
     r = f(geteclass(g, match.id), match.sub, g, actual_params...)
     rc, node = addexpr!(g, r)
-
-    return (nothing, (match.id, rc.id))
+    merge!(g, match.id, rc.id)
+    return nothing
 end
 
 
 using .ReportReasons
 function eqsat_apply!(g::EGraph, matches::MatchesBuf, rep::Report, params::SaturationParams)
     i = 0
-
-    unions = UnionBuf()
     # println.(matches)
     for match ∈ matches
         i += 1
 
-        # if i % 300 == 0
         if params.eclasslimit > 0 && g.numclasses > params.eclasslimit
             @log "E-GRAPH SIZEOUT"
             rep.reason = EClassLimit(params.eclasslimit)
@@ -56,20 +52,15 @@ function eqsat_apply!(g::EGraph, matches::MatchesBuf, rep::Report, params::Satur
         # println("applying $rule")
         # @show match.sub.sourcenode
 
-        halt_reason, union = rule(g, match)
+        halt_reason = rule(g, match)
         if (halt_reason !== nothing)
             rep.reason = halt_reason
             return 
         end 
-        (union !== nothing) && push!(unions, union)
 
         # println(rule)
         # println(sub)
         # println(l); println(r)
         # display(egraph.classes); println()
-    end
-
-    for (l,r) ∈ unions 
-        merge!(g, l, r)
     end
 end
