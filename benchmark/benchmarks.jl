d34daa13edf22fb09a08cbb4c92c96cc4c61c05a
Alessandro <none>
2022-01-10 12:45:51 +0100

    initialized benchmarks

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -5,4 +5,8 @@ using Metatheory
 SUITE = BenchmarkGroup()
 
 egraph = SUITE["egraph"] = BenchmarkGroup()
-egraph["creation"] = @benchmarkable EGraph()
+
+# E-Graph creation
+egraph["creation"] = BenchmarkGroup()
+egraph["creation"]["empty"] = @benchmarkable EGraph()
+egraph["creation"]["expr"] = @benchmarkable EGraph(:(a + b^2 / (x^2 - 123 + Dict(:x => 2))))
