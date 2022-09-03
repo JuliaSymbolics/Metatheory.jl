2188fc94ed55f27e07021bf5bc85baf7404c4c32
Dani Pinyol <dani@avatarcognition.com>
2022-09-03 07:53:17 +0200

    remove deprecated _iszero/_isone/_isinteger/_isreal
    They're not used anymore and _isinteger/_isreal would fail because SymbolicUtils is not used anymore

--- src/utils.jl
+++ src/utils.jl
@@ -81,12 +81,6 @@ end
 isliteral(::Type{T}) where {T} = x -> x isa T
 is_literal_number(x) = isliteral(Number)(x)
 
-# checking the type directly is faster than dynamic dispatch in type unstable code
-_iszero(x) = x isa Number && iszero(x)
-_isone(x) = x isa Number && isone(x)
-_isinteger(x) = (x isa Number && isinteger(x)) || (x isa Symbolic && symtype(x) <: Integer)
-_isreal(x) = (x isa Number && isreal(x)) || (x isa Symbolic && symtype(x) <: Real)
-
 issortedₑ(args) = issorted(args, lt=<ₑ)
 needs_sorting(f) = x -> is_operation(f)(x) && !issortedₑ(arguments(x))
 
