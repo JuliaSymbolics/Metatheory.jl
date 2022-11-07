88891adbe7541144a71662af3c5d8d638990afa5
Alessandro <none>
2022-11-07 20:25:38 +0100

    auto format

--- benchmarks/egg_logic.jl
+++ benchmarks/egg_logic.jl
@@ -51,14 +51,14 @@ fold = @theory begin
   (false == true)  => false
   (true == true)   => true
   (false == false) => true
-  (true || false)   => true
-  (false || true)   => true
-  (true || true)    => true
-  (false || false)  => false
-  (true && true)    => true
-  (false && true)   => false
-  (true && false)   => false
-  (false && false)  => false
+  (true || false)  => true
+  (false || true)  => true
+  (true || true)   => true
+  (false || false) => false
+  (true && true)   => true
+  (false && true)  => false
+  (true && false)  => false
+  (false && false) => false
   !(true)          => false
   !(false)         => true
 end
