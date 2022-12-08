89aff78e3b4ef718a92bd3056680d3bae8ae2105
Alessandro <none>
2022-12-08 15:05:05 +0000

    adjust tutorials tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -14,7 +14,7 @@ end
 
 const TEST_FILES = ["reductions.jl", "EGraphs/egraphs.jl", "EGraphs/ematch.jl", "EGraphs/analysis.jl"]
 const INTEGRATION_TEST_FILES = map(
-  x -> joinpath("integration", x),
+  x -> joinpath(@__DIR__, "integration", x),
   [
     "custom_types.jl",
     "fibonacci.jl",
@@ -23,15 +23,17 @@ const INTEGRATION_TEST_FILES = map(
     "mu.jl",
     "stream_fusion.jl",
     "taylor.jl",
-    "while_interpreter.jl",
     "while_superinterpreter.jl",
     "lambda_theory.jl"
   ],
 )
 
+const TUTORIALS = [joinpath(@__DIR__, "tutorials", x) for x in readdir("tutorials/") if endswith(x, ".jl")]
+
 @timev begin
   @timev map(test, TEST_FILES)
   @timev map(test, INTEGRATION_TEST_FILES)
+  @timev map(test, TUTORIALS)
 end
 
 # exported consistency test
