e2a69776ddcea21e0653de51a5bdcb0f82aa33d2
a <a@a.a>
2023-12-07 10:21:49 +0100

    add package benchmarks

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -6,7 +6,7 @@ const SUITE = BenchmarkGroup()
 
 function simplify(ex, theory, params = SaturationParams(), postprocess = identity)
   g = EGraph(ex)
-  report = saturate!(g, cas, params)
+  report = saturate!(g, theory, params)
   println(report)
   res = extract!(g, astsize)
   postprocess(res)
@@ -14,8 +14,21 @@ end
 
 
 include("maths_theory.jl")
+include("logic_theory.jl")
+
 
 SUITE["maths"] = BenchmarkGroup(["egraphs"])
 
-ex1 = :(a + b + (0 * c) + d)
-SUITE["maths"]["simpl1"] = @benchmarkable simplify($ex1, $maths_theory, $(SaturationParams()), postprocess_maths)
+ex = :(a + b + (0 * c) + d)
+SUITE["maths"]["simpl1"] = @benchmarkable simplify($ex, $maths_theory, $(SaturationParams()), postprocess_maths)
+
+# ==================================================================
+
+SUITE["logic"] = BenchmarkGroup(["egraph", "logic"])
+
+ex_orig = :(((p ⟹ q) && (r ⟹ s) && (p || r)) ⟹ (q || s))
+ex = rewrite(ex_orig, impl)
+
+SUITE["logic"]["rewrite"] = @benchmarkable rewrite($ex_orig, $impl)
+SUITE["logic"]["prove1"] = @benchmarkable prove($maths_theory, $ex, 5, 10, 5000)
+
