5022ce0eac08f6262c4c2bb8761fe7e31069bb1a
a <a@a.a>
2023-12-10 15:09:56 +0100

    adjust tests and benchmarks

--- test/integration/logic.jl
+++ test/integration/logic.jl
@@ -22,7 +22,7 @@ function prove(t, ex, steps = 1, timeout = 10, eclasslimit = 5000)
     params.goal = (g::EGraph) -> in_same_class(g, ids...)
     saturate!(g, t, params)
     ex = extract!(g, astsize)
-    if !istree(ex)
+    if !Metatheory.istree(ex)
       return ex
     end
     if hash(ex) ∈ hist
