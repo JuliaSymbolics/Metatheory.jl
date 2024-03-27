bfb555a940f952f002b9cf8223060225e86e1219
Niklas Heim <heim.niklas@gmail.com>
2024-03-27 14:50:01 +0100

    some more doc strings

--- src/vecexpr.jl
+++ src/vecexpr.jl
@@ -28,8 +28,8 @@ const Id = UInt64
 
 An e-node is a `Vector{ID}` where:
 * Position 1 stores the hash of the `VecExpr`.
-* Position 2 stores the bit flags (`istree` or `iscall`).
-* Position 3 stores the index of the `head` (if `istree`) or value in the e-graph constants.
+* Position 2 stores the bit flags (`isexpr` or `iscall`).
+* Position 3 stores the index of the `head` (if `isexpr`) or value in the e-graph constants.
 * The rest of the positions store the e-class ids of the children nodes.
 """
 const VecExpr = Vector{Id}
@@ -43,9 +43,13 @@ const VECEXPR_META_LENGTH = 3
 @inline v_check_flags(n::VecExpr, flag::Id)::Bool = !iszero(v_flags(n) & flags)
 @inline v_set_flag!(n::VecExpr, flag)::Id = @inbounds (n[2] = n[2] | flag)
 
+"""Returns `true` if the e-node ID points to a an expression tree."""
 @inline v_isexpr(n::VecExpr)::Bool = !iszero(v_flags(n) & VECEXPR_FLAG_ISTREE)
+
+"""Returns `true` if the e-node ID points to a function call."""
 @inline v_iscall(n::VecExpr)::Bool = !iszero(v_flags(n) & VECEXPR_FLAG_ISCALL)
 
+"""Number of children in the e-node."""
 @inline v_arity(n::VecExpr)::Int = length(n) - VECEXPR_META_LENGTH
 
 """
@@ -61,13 +65,22 @@ Compute the hash of a `VecExpr` and store it as the first element.
   end
 end
 
+"""The hash of the e-node."""
 @inline v_hash(n::VecExpr)::Id = @inbounds n[1]
+
+"""Set e-node hash to zero."""
 @inline v_unset_hash!(n::VecExpr)::Id = @inbounds (n[1] = Id(0))
 
+"""E-class IDs of the children of the e-node."""
 @inline v_children(n::VecExpr) = @view n[(VECEXPR_META_LENGTH + 1):end]
+
+"""E-node ID."""
 @inline v_head(n::VecExpr)::Id = @inbounds n[VECEXPR_META_LENGTH]
+
+"""Update the E-node ID."""
 @inline v_set_head!(n::VecExpr, h::Id) = @inbounds (n[3] = h)
 
+"""Construct a new, empty `VecExpr` with `len` children."""
 @inline function v_new(len::Int)::VecExpr
   n = Vector{Id}(undef, len + VECEXPR_META_LENGTH)
   v_unset_hash!(n)
