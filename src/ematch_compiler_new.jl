a5cf420083a226bb76a45653f900376d3b1e0994
a <a@a.a>
2024-04-20 20:34:26 +0200

    ITS FKN WORKING

--- src/ematch_compiler_new.jl
+++ src/ematch_compiler_new.jl
@@ -1,39 +1,64 @@
 # Full e-matcher
-function ematch_compile(p)
+function ematch_compile(p, direction)
   pvars = patvars(p)
   npvars = length(pvars)
 
   patvar_to_addr = fill(-1, npvars)
+  ground_terms_to_addr = Dict{Any,Int}()
 
   program = Expr[]
+  memsize = Ref(1)
 
-  memsize = 1
-  ematch_compile_ground!(program, memsize)
-  first_nonground = memsize
-  memsize += 1
+  ematch_compile_ground!(1, ground_terms_to_addr, program, memsize, p)
+  first_nonground = memsize[]
+  memsize[] += 1
 
-  ematch_compile!(program, p, memsize, first_nonground)
+  #   ematch_compile!(program, p, memsize, first_nonground)
+  #   There is only a ground pattern 
+  if first_nonground == memsize[] - 1 && isground(p)
+    push!(program, :(
+      if root_id == σ[1]
+        pc += 1
+        @goto compute
+      else
+        @goto backtrack
+      end
+    ))
+  end
 
-
-  push!(prog, yield_expr(patvar_to_addr, direction))
-
-  σ = fill(-1, memsize)
+  push!(program, yield_expr(patvar_to_addr, direction))
+  σ = fill(-1, memsize[])
   quote
-    function (g::EGraph, rule_idx::Int, root_id::Id)::Int
+    function ematch_this(g::EGraph, rule_idx::Int, root_id::Id)::Int
+      σ = $σ
       n_matches = 0
       stack = Int[]
       push!(stack, 0)
+      pc = 1
 
       @label compute
 
       pc == 0 && return n_matches
 
       $([:(
-        if pc == $i
-          $code
+        begin
+          @show σ
+          println("CURRENT PC = $pc")
+          if pc == $i
+            $code
+          end
         end
-      ) for (i, code) in enumerate(prog)]...)
-      ...
+      ) for (i, code) in enumerate(program)]...)
+
+      error("unreachable code!")
+
+      @label backtrack
+      @show "BACKTRACKING"
+      @show stack
+      pc = pop!(stack)
+      @goto compute
+
+      return -1
     end
   end
 
@@ -44,7 +69,7 @@ end
 # TODO explain what is a ground term
 # ==============================================================
 
-function compile_ground!(addr, ground_terms_to_addr, program, memsize, p::Any)
+function ematch_compile_ground!(addr, ground_terms_to_addr, program, memsize, p::Any)
   if !haskey(ground_terms_to_addr, p)
     # If the instruction for searching the constant literal
     # has not already been inserted in the program: 
@@ -56,7 +81,7 @@ function compile_ground!(addr, ground_terms_to_addr, program, memsize, p::Any)
 end
 
 # Ground e-matchers
-function compile_ground!(addr, ground_terms_to_addr, program, memsize, pattern::PatTerm)
+function ematch_compile_ground!(addr, ground_terms_to_addr, program, memsize, pattern::PatExpr)
   if !haskey(ground_terms_to_addr, pattern)
     # If the instruction for searching the term
     # has not already been inserted in the program:
@@ -72,7 +97,7 @@ function compile_ground!(addr, ground_terms_to_addr, program, memsize, pattern::
     else
       # Search for ground patterns in the children.
       for child_pattern in children(pattern)
-        compile_ground!.(memsize[], ground_terms_to_addr, program, memsize, child_pattern)
+        ematch_compile_ground!.(memsize[], ground_terms_to_addr, program, memsize, child_pattern)
       end
     end
   end
@@ -84,7 +109,7 @@ end
 
 function lookup_expr(addr, p)
   quote
-    ecid = lookup_pat(g, $p)
+    ecid = Metatheory.EGraphs.lookup_pat(g, $p)
     if ecid > 0
       σ[$addr] = ecid
       pc += 1
@@ -95,17 +120,14 @@ function lookup_expr(addr, p)
 end
 
 function yield_expr(patvar_to_addr, direction)
-
-  makedict = [:(b = assoc(b, $i, (σ[$addr], n[$addr]))) for (i, addr) in enumerate(patvar_to_addr)]...
+  makedict = [:(b = assoc(b, $i, (σ[$addr], n[$addr]))) for (i, addr) in enumerate(patvar_to_addr)]
   quote
-    maybelock!(g) do
-      b = Bindings()
+    Metatheory.EGraphs.maybelock!(g) do
+      b = Metatheory.Bindings()
       $(makedict...)
-
-      push!(g.buffer, assoc(b, 0, (root_id, rule_idx * $direction)))
+      push!(g.buffer, Metatheory.assoc(b, 0, (root_id, rule_idx * $direction)))
       n_matches += 1
     end
-
     @goto backtrack
   end
 end
