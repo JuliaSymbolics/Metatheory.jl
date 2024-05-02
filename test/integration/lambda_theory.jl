ef0ed6d8c0758d1442abebe85c20335b9604d963
Niklas Heim <heim.niklas@gmail.com>
2024-05-02 21:43:03 +0200

    include fresh var test

--- test/integration/lambda_theory.jl
+++ test/integration/lambda_theory.jl
@@ -38,41 +38,43 @@ end
   y
 end
 
+const LambdaAnalysis = Set{Symbol}
 
-function TermInterface.maketerm(::Type{LambdaExpr}, head, children; type = nothing, metadata = nothing)
+function TermInterface.maketerm(::Type{<:LambdaExpr}, head, children; type = nothing, metadata = nothing)
   head(children...)
 end
 
-function EGraphs.make(::Val{:freevar}, g::EGraph, n::VecExpr)
-  free = Set{Int64}()
-  v_isexpr(n) || return free
+getdata(eclass) = isnothing(eclass.data) ? LambdaAnalysis() : eclass.data
+
+function EGraphs.make(g::EGraph{ExprType,LambdaAnalysis}, n::VecExpr) where ExprType
+  v_isexpr(n) || LambdaAnalysis()
   if v_iscall(n)
-    op = get_constant(g, v_head(n))
+    h = v_head(n)
+    op = get_constant(g, h)
     args = v_children(n)
+    eclass = g[args[1]]
+    free = getdata(eclass)
 
     if op == Variable
-      push!(free, args[1])
+      push!(free, get_constant(g, v_head(eclass.nodes[1])))
     elseif op == Let
       v, a, b = args[1:3]
-      adata = getdata(g[a], :freevar, Set{Int64}())
-      bdata = getdata(g[a], :freevar, Set{Int64}())
+      adata = getdata(g[a])
+      bdata = getdata(g[b])
       union!(free, adata)
       delete!(free, v)
       union!(free, bdata)
     elseif op == λ
       v, b = args[1:2]
-      bdata = getdata(g[b], :freevar, Set{Int64}())
+      bdata = getdata(g[b])
       union!(free, bdata)
       delete!(free, v)
     end
+    return free
   end
-
-  return free
 end
 
-EGraphs.join(::Val{:freevar}, from, to) = union(from, to)
-
-islazy(::Val{:freevar}) = false
+EGraphs.join(from::LambdaAnalysis, to::LambdaAnalysis) = union(from,to)
 
 open_term = @theory x e then alt a b c begin
   # if-true 
@@ -108,35 +110,48 @@ subst_prop = @theory v e a b then alt guard begin
 end
 
 
+isfree(eclass, id) = id ∈ getdata(eclass)
+
+function fresh_var_generator()
+  idx = 0
+  function generate()
+    idx += 1
+    Symbol("a$idx")
+  end
+end
+
+freshvar = fresh_var_generator()
+
 subst_elim = @theory v e c v1 v2 body begin
   # let-const 
   Let(v, e, c::Any) --> c
   # let-Variable-same 
   Let(v1, e, Variable(v1)) --> e
   # TODO fancy let-Variable-diff 
-  Let(v1, e, Variable(v2)) => if addexpr!(_egraph, v1) != addexpr!(_egraph, v2)
-    :(Variable($v2))
-  else
-    _lhs_expr
-  end
+  Let(v1, e, Variable(v2)) => v1 == v2 ? e : Variable(v2)
   # let-lam-same 
   Let(v1, e, λ(v1, body)) --> λ(v1, body)
   # let-lam-diff #TODO captureavoid
-  Let(v1, e, λ(v2, body)) => if v2.id ∈ getdata(e, :freevar, Set()) # is free
-    :(λ($fresh, Let($v1, $e, Let($v2, Variable($fresh), $body))))
+  Let(v1, e, λ(v2, body)) => if isfree(e,v2)
+    fresh = freshvar()
+    λ(fresh, Let(v1, e, Let(v2, Variable(fresh), body)))
   else
-    :(λ($v2, Let($v1, $e, $body)))
+    λ(v2, Let(v1, e, body))
   end
 end
 
-λT = open_term ∪ subst_intro ∪ subst_prop ∪ subst_elim
+λT = subst_intro ∪ subst_prop ∪ subst_elim
+#λT = open_term ∪ subst_intro ∪ subst_prop ∪ subst_elim
 
-ex = λ(:x, Add(4, Apply(λ(:y, Variable(:y)), 4)))
-g = EGraph{LambdaExpr}(ex)
-
+x = Variable(:x)
+y = Variable(:y)
+ex = Apply(λ(:x, λ(:y, Apply(x,y))), y)
+g = EGraph{LambdaExpr,LambdaAnalysis}(ex)
 saturate!(g, λT)
-@test λ(:x, Add(4, 4)) == extract!(g, astsize) # expected: :(λ(x, 4 + 4))
+@test λ(:a4, Apply(y, Variable(:a4))) == extract!(g, astsize)
 
-#%%
-g = EGraph{LambdaExpr}()
-@test areequal(g, λT, 2, Apply(λ(:x, Variable(:x)), 2))
+ex = λ(:x, Add(4, Apply(λ(:y, y), 4)))
+g = EGraph{LambdaExpr,LambdaAnalysis}(ex)
+saturate!(g, λT)
+r = extract!(g, astsize)
+@test λ(:x, Add(4, 4)) == extract!(g, astsize) # expected: :(λ(x, 4 + 4))
