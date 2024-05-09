097d49a8a1f151d66168f02a7297481ffd8540e7
Niklas Heim <heim.niklas@gmail.com>
2024-05-09 16:13:15 +0200

    fix calc logic rules

--- examples/propositional_logic_theory.jl
+++ examples/propositional_logic_theory.jl
@@ -52,39 +52,3 @@ impl = @theory p q begin
 end
 
 propositional_logic_theory = or_alg ∪ and_alg ∪ comb ∪ negt ∪ impl ∪ fold
-
-
-# Sketch function for basic iterative saturation and extraction 
-function prove(
-  t,
-  ex,
-  steps = 1,
-  timeout = 10,
-  params = SaturationParams(
-    timeout = timeout,
-    scheduler = Schedulers.BackoffScheduler,
-    schedulerparams = (6000, 5),
-    timer = false,
-  ),
-)
-  hist = UInt64[]
-  push!(hist, hash(ex))
-  for i in 1:steps
-    g = EGraph(ex)
-
-    ids = [addexpr!(g, true), g.root]
-
-    params.goal = (g::EGraph) -> in_same_class(g, ids...)
-    saturate!(g, t, params)
-    ex = extract!(g, astsize)
-    if !TermInterface.isexpr(ex)
-      return ex
-    end
-    if hash(ex) ∈ hist
-      return ex
-    end
-    push!(hist, hash(ex))
-  end
-  return ex
-end
-
