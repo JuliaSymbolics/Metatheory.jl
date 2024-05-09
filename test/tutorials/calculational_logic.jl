86eaf1f4e199cac144fe988a7a993bce7ab10ca6
Niklas Heim <heim.niklas@gmail.com>
2024-05-09 16:14:19 +0200

    prove in separate file

--- test/tutorials/calculational_logic.jl
+++ test/tutorials/calculational_logic.jl
@@ -1,6 +1,7 @@
 # # Rewriting Calculational Logic
 using Metatheory, Test
 
+include(joinpath(dirname(pathof(Metatheory)), "../examples/prove.jl"))
 include(joinpath(dirname(pathof(Metatheory)), "../examples/calculational_logic_theory.jl"))
 
 
@@ -13,16 +14,16 @@ include(joinpath(dirname(pathof(Metatheory)), "../examples/calculational_logic_t
   @test @areequal calculational_logic_theory ((!p == !p) == true) true
   @test @areequal calculational_logic_theory ((!p || !p) == !p) (!p || p) !(!p && p) true
   @test @areequal calculational_logic_theory ((p ⟹ (p || p)) == true) true
-  params = SaturationParams(timeout = 12, eclasslimit = 10000, schedulerparams = (1000, 5))
 
+  params = SaturationParams(timeout = 12, eclasslimit = 10000, schedulerparams = (1000, 5))
   @test areequal(calculational_logic_theory, :(((p ⟹ (p || p)) == ((!(p) && q) ⟹ q))), true; params = params)
 
   ex = :((p ⟹ (q ⟹ r)) ⟹ ((p ⟹ q) ⟹ (p ⟹ r)))   # Frege's theorem
-  res = areequal(calculational_logic_theory, ex, true; params = params)
-  @test_broken !ismissing(res) && res
-
+  params = SaturationParams(timeout = 12, eclasslimit = 10000, schedulerparams = (6000, 5))
+  @test prove(calculational_logic_theory, ex, 2, 10, params)
 
   @test @areequal calculational_logic_theory (!(p || q) == (!p && !q)) true   # Demorgan's
 
-  areequal(calculational_logic_theory, :((x && y) || (!x && z) || (y && z)), :((x && y) || (!x && z)); params = params)   # Consensus theorem
+  # TODO: fix this one as well?
+  # @test prove(calculational_logic_theory, :((x && y) || (!x && z) || (y && z)), :((x && y) || (!x && z)), 2, 10, params)   # Consensus theorem
 end
