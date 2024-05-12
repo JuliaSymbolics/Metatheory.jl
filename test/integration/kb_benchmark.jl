78871dcf215a91f894d9b1bb5c55904f356926a0
a <a@a.a>
2024-05-12 20:15:46 +0200

    adjust benchmark

--- test/integration/kb_benchmark.jl
+++ test/integration/kb_benchmark.jl
@@ -29,42 +29,60 @@ Massoc = @theory a b c begin
 end
 
 
-T = [
-  @rule :b * :B --> :ε
-  @rule :a * :a --> :ε
-  @rule (:b * :b) * :b --> :ε
-  @rule :B * :B --> :B
-  @rule (@rep (:a * :b) (*) 7) --> :ε
-  @rule (@rep (:a * :b * :a * :B) (*) 7) --> :ε
-]
+macro kb_theory_237_abab(n)
+  quote
+    T = [
+      @rule :b * :B --> :ε
+      @rule :a * :a --> :ε
+      @rule (:b * :b) * :b --> :ε
+      @rule :B * :B --> :B
+      @rule (@rep (:a * :b) (*) 7) --> :ε
+      @rule (@rep (:a * :b * :a * :B) (*) $n) --> :ε
+    ]
+    group_theory = Mid ∪ Massoc ∪ T
+  end
+end
 
-G = Mid ∪ Massoc ∪ T
+@kb_theory_237_abab 5
 
 astsize_prefer_empty(n::VecExpr, op, costs)::Float64 = op == :ε ? 0 : astsize(n, op, costs)
 
-function test_kb(expr, params = SaturationParams())
+function test_kb(expr, t, params = SaturationParams())
   g = EGraph(expr)
-  saturate!(g, G, params)
+  saturate!(g, t, params)
   ex = extract!(g, astsize_prefer_empty)
+
+  # Check if group is trivial
+  # a = addexpr!(g, :a)
+  # b = addexpr!(g, :b)
+  # B = addexpr!(g, :B)
+  # ε = addexpr!(g, :ε)
+  # @show in_same_class(g, a, ε)
+  # @show in_same_class(g, b, ε)
+  # @show in_same_class(g, B, ε)
   ex == :ε
 end
 
-@test test_kb(:(b * B))
-@test test_kb(:(a * a * a * a))
-@test test_kb(:(((((((a * b) * (a * b)) * (a * b)) * (a * b)) * (a * b)) * (a * b)) * (a * b)))
-@test test_kb(
-  :(a * b * a * a * a * b * b * b * a * B * B * B * B * a),
-  SaturationParams(timeout = 5, scheduler = SimpleScheduler),
-)
-
-@test !test_kb(:(((((((a * b) * (a * b)) * (a * b)) * (a * b)) * (a * b)) * (a * b)) * (b * a)))
-@test !test_kb(:(a * a * b * a))
-@test !test_kb(
-  :(a * b * b * a * a * b * b * b * a * B * B * B * B * a),
-  SaturationParams(timeout = 5, scheduler = SimpleScheduler),
-)
 
 
 
+for n in 5:8
+  t = @eval @kb_theory_237_abab $n
 
+  @test test_kb(:(b * B), group_theory)
+  @test test_kb(:(a * a * a * a), group_theory)
+  @test test_kb(:(((((((a * b) * (a * b)) * (a * b)) * (a * b)) * (a * b)) * (a * b)) * (a * b)), group_theory)
+  @test test_kb(
+    :(a * b * a * a * a * b * b * b * a * B * B * B * B * a),
+    group_theory,
+    SaturationParams(timeout = 5, scheduler = SimpleScheduler),
+  )
 
+  @test !test_kb(:(((((((a * b) * (a * b)) * (a * b)) * (a * b)) * (a * b)) * (a * b)) * (b * a)), group_theory)
+  @test !test_kb(:(a * a * b * a), group_theory)
+  @test !test_kb(
+    :(a * b * b * a * a * b * b * b * a * B * B * B * B * a),
+    group_theory,
+    SaturationParams(timeout = 5, scheduler = SimpleScheduler),
+  )
+end
