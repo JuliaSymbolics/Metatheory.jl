dfd468f9cdb8c3b6f2d3d6545a5bdc424c4036f3
Owen Lynch <root@owenlynch.org>
2024-08-16 16:01:34 -0700

    2-space indent

--- src/ematch_compiler.jl
+++ src/ematch_compiler.jl
@@ -12,7 +12,7 @@ if pc === 1
 else if pc === 2
   # do instruction 2
 else
- ...
+...
 end
 ```
 
@@ -58,18 +58,18 @@ enode_var(addr::Address) = Symbol(:enode_idx, addr)
 abstract type Instruction end
 
 function set_backtrack_checkpoint()
-    :(push!(stack, pc))
+  :(push!(stack, pc))
 end
 
 function continue_()
-    quote
-        pc += 0x0001
-        @goto compute
-    end
+  quote
+    pc += 0x0001
+    @goto compute
+  end
 end
 
 function backtrack()
-    :(@goto backtrack)
+  :(@goto backtrack)
 end
 
 """
@@ -82,55 +82,55 @@ at `enode_var(addr)`. The eids of the arguments are available at
 `[eclass_var(addr) for addr in memrange]`.
 """
 struct BindExpr <: Instruction
-    addr::Address
-    flags::Id
-    signature::Id
-    head_hash::Id
-    quoted_head_hash::Id
-    memrange::UnitRange{Int}
+  addr::Address
+  flags::Id
+  signature::Id
+  head_hash::Id
+  quoted_head_hash::Id
+  memrange::UnitRange{Int}
 end
 
 function BindExpr(addr::Address, p::PatExpr, memrange::UnitRange{Int})
-    BindExpr(
-        addr,
-        v_flags(p.n),
-        v_signature(p.n),
-        p.head_hash,
-        p.quoted_head_hash,
-        memrange
-    )
+  BindExpr(
+    addr,
+    v_flags(p.n),
+    v_signature(p.n),
+    p.head_hash,
+    p.quoted_head_hash,
+    memrange
+  )
 end
 
 function to_expr(inst::BindExpr)
-    addr = inst.addr
-    i = enode_var(addr)
-    check_flags = :(v_flags(n) === $(inst.flags))
-    check_sig = :(v_flags(n) === $(inst.flags))
-    check_head = :(v_head(n) === $(inst.head_hash) || v_head(n) === $(inst.quoted_head_hash))
-    quote
-        eclass = g[$(eclass_var(addr))]
-        eclass_length = length(eclass.nodes)
+  addr = inst.addr
+  i = enode_var(addr)
+  check_flags = :(v_flags(n) === $(inst.flags))
+  check_sig = :(v_flags(n) === $(inst.flags))
+  check_head = :(v_head(n) === $(inst.head_hash) || v_head(n) === $(inst.quoted_head_hash))
+  quote
+    eclass = g[$(eclass_var(addr))]
+    eclass_length = length(eclass.nodes)
 
-        if $i <= eclass_length
-            $(set_backtrack_checkpoint())
+    if $i <= eclass_length
+      $(set_backtrack_checkpoint())
 
-            n = eclass.nodes[$i]
+      n = eclass.nodes[$i]
 
-            $i += 1
-            if $check_flags && $check_sig && $check_head
-                $((map(enumerate(inst.memrange)) do (idx, addr)
-                    :($(eclass_var(addr)) = v_children(n)[$idx])
-                end)...)
+      $i += 1
+      if $check_flags && $check_sig && $check_head
+        $((map(enumerate(inst.memrange)) do (idx, addr)
+          :($(eclass_var(addr)) = v_children(n)[$idx])
+        end)...)
 
-                $(continue_())
-            else
-                $(backtrack())
-            end
-        else
-            $i = 1
-            $(backtrack())
-        end
+        $(continue_())
+      else
+        $(backtrack())
+      end
+    else
+      $i = 1
+      $(backtrack())
     end
+  end
 end
 
 """
@@ -140,56 +140,56 @@ If so, also set `enode_var(addr)` to be one after the first enode
 that is not an expression? This doesn't really make sense to me.
 """
 struct CheckVar <: Instruction
-    addr::Address
-    predicate::Union{Function, Type}
+  addr::Address
+  predicate::Union{Function, Type}
 end
 
 function to_expr(inst::CheckVar)
-    addr = inst.addr
-    if inst.predicate isa Function
-        quote
-            eclass = g[$(eclass_var(addr))]
-            if ($inst.predicate)(g, eclass)
-                for (j, n) in enumerate(eclass.nodes)
-                    if !v_isexpr(n)
-                        $(enode_var(addr)) = j + 1
-                        break
-                    end
-                end
-                $(continue_())
-            else
-                $(backtrack())
-            end
+  addr = inst.addr
+  if inst.predicate isa Function
+    quote
+      eclass = g[$(eclass_var(addr))]
+      if ($inst.predicate)(g, eclass)
+        for (j, n) in enumerate(eclass.nodes)
+          if !v_isexpr(n)
+            $(enode_var(addr)) = j + 1
+            break
+          end
         end
-    elseif inst.predicate isa Type
-        T = inst.predicate
-        i = enode_var(addr)
-        quote
-            eclass = g[$(eclass_var(addr))]
-            eclass_length = length(eclass.nodes)
-            if $i <= eclass_length
-                $(set_backtrack_checkpoint())
-
-                n = eclass.nodes[$i]
-
-                if !v_isexpr(n)
-                    hn = Metatheory.EGraphs.get_constant(g, v_head(n))
-                    if hn isa $T
-                        $i += 1
-                        $(continue_())
-                    end
-                end
-
-                # This node did not match. Try next node and backtrack.
-                $i += 1
-                $(backtrack())
-            end
-
-            # Restart from first option
-            $(Symbol(:enode_idx, addr)) = 1
-            @goto backtrack
-        end
+        $(continue_())
+      else
+        $(backtrack())
+      end
     end
+  elseif inst.predicate isa Type
+    T = inst.predicate
+    i = enode_var(addr)
+    quote
+      eclass = g[$(eclass_var(addr))]
+      eclass_length = length(eclass.nodes)
+      if $i <= eclass_length
+        $(set_backtrack_checkpoint())
+
+        n = eclass.nodes[$i]
+
+        if !v_isexpr(n)
+          hn = Metatheory.EGraphs.get_constant(g, v_head(n))
+          if hn isa $T
+            $i += 1
+            $(continue_())
+          end
+        end
+
+        # This node did not match. Try next node and backtrack.
+        $i += 1
+        $(backtrack())
+      end
+
+      # Restart from first option
+      $(Symbol(:enode_idx, addr)) = 1
+      @goto backtrack
+    end
+  end
 end
 
 """
@@ -197,18 +197,18 @@ Checks that the two e-ids in `eclass_var(addr_a)` and `eclass_var(addr_b)` are
 equal, continuing if so and backtracking otherwise.
 """
 struct CheckEq <: Instruction
-    addr_a::Address
-    addr_b::Address
+  addr_a::Address
+  addr_b::Address
 end
 
 function to_expr(inst::CheckEq)
-    quote
-        if $(eclass_var(inst.addr_a)) == $(eclass_var(inst.addr_b))
-            $(continue_())
-        else
-            $(backtrack())
-        end
+  quote
+    if $(eclass_var(inst.addr_a)) == $(eclass_var(inst.addr_b))
+      $(continue_())
+    else
+      $(backtrack())
     end
+  end
 end
 
 """
@@ -219,24 +219,24 @@ Checks if the e-graph contains an instance of `p`, and if so, loads the e-id for
 `p` into `eclass_var(addr)`.
 """
 struct Lookup <: Instruction
-    addr::Int
-    p::AbstractPat
-    function Lookup(addr::Int, p::AbstractPat)
-        @assert isground(p)
-        new(addr, p)
-    end
+  addr::Int
+  p::AbstractPat
+  function Lookup(addr::Int, p::AbstractPat)
+    @assert isground(p)
+    new(addr, p)
+  end
 end
 
 function to_expr(inst::Lookup)
-    quote
-        ecid = lookup_pat(g, $(inst.p))
-        if ecid > 0
-            $(eclass_var(inst.addr)) = ecid
-            $(continue_())
-        else
-            $(backtrack())
-        end
+  quote
+    ecid = lookup_pat(g, $(inst.p))
+    if ecid > 0
+      $(eclass_var(inst.addr)) = ecid
+      $(continue_())
+    else
+      $(backtrack())
     end
+  end
 end
 
 """
@@ -248,142 +248,142 @@ found at the end of a match process, and the corresponding e-nodes that were
 selected for those e-ids.
 """
 struct Yield <: Instruction
-    patvar_to_addr::Vector{Address}
-    direction::Int
+  patvar_to_addr::Vector{Address}
+  direction::Int
 end
 
 function to_expr(inst::Yield)
-    push_exprs = [
-        :(push!(
-            ematch_buffer,
-            v_pair(
-                $(eclass_var(addr)),
-                reinterpret(UInt64, $(enode_var(addr)) - 1)
-            )
-        )) for addr in inst.patvar_to_addr
-    ]
-    quote
-        g.needslock && lock(g.lock)
-        push!(ematch_buffer, v_pair(root_id, reinterpret(UInt64, rule_idx * $(inst.direction))))
-        $(push_exprs...)
-        push!(ematch_buffer, 0xffffffffffffffffffffffffffffffff)
-        n_matches += 1
-        g.needslock && unlock(g.lock)
-        $(backtrack())
-    end
+  push_exprs = [
+               :(push!(
+                 ematch_buffer,
+                 v_pair(
+                   $(eclass_var(addr)),
+                   reinterpret(UInt64, $(enode_var(addr)) - 1)
+                 )
+               )) for addr in inst.patvar_to_addr
+  ]
+  quote
+    g.needslock && lock(g.lock)
+    push!(ematch_buffer, v_pair(root_id, reinterpret(UInt64, rule_idx * $(inst.direction))))
+    $(push_exprs...)
+    push!(ematch_buffer, 0xffffffffffffffffffffffffffffffff)
+    n_matches += 1
+    g.needslock && unlock(g.lock)
+    $(backtrack())
+  end
 end
 
 Base.@kwdef mutable struct EMatchCompilerState
-    """
-    Ground terms are matched at the beginning.
+  """
+  Ground terms are matched at the beginning.
 
-    This is the index of the σ variable (address) that represents the first
-    non-ground term.
+  This is the index of the σ variable (address) that represents the first
+  non-ground term.
 
-    In other words, this is the address of the root pattern that we are
-    matching.
-    """
-    first_nonground::Int = 0
+  In other words, this is the address of the root pattern that we are
+  matching.
+  """
+  first_nonground::Int = 0
 
-    """
-    This provides a lookup for the address corresponding to each ground term.
-    """
-    ground_terms_to_addr::Dict{AbstractPat, Int} = Dict{AbstractPat,Int}()
+                         """
+                         This provides a lookup for the address corresponding to each ground term.
+                         """
+  ground_terms_to_addr::Dict{AbstractPat, Int} = Dict{AbstractPat,Int}()
 
-    """
-    Given a pattern variable with Debrujin index i
-    This vector stores the σ variable index (address) for that variable at position i
+  """
+  Given a pattern variable with Debrujin index i
+  This vector stores the σ variable index (address) for that variable at position i
 
-    This is a vector rather than a Dict... because we Debruijn-index pattern
-    variables.
-    """
-    patvar_to_addr::Vector{Address} = Address[]
+  This is a vector rather than a Dict... because we Debruijn-index pattern
+  variables.
+  """
+  patvar_to_addr::Vector{Address} = Address[]
 
-    """
-    For some addresses, we care not just about the e-class in the address but also
-    the e-node in that e-class. This stores the addresses for which that is the
-    case.
-    """
-    enode_var_addresses::Vector{Address} = Address[]
+  """
+  For some addresses, we care not just about the e-class in the address but also
+  the e-node in that e-class. This stores the addresses for which that is the
+  case.
+  """
+  enode_var_addresses::Vector{Address} = Address[]
 
-    """
-    The program, encoded as a list of instructions
-    """
-    program::Vector{Instruction} = Instruction[]
+  """
+  The program, encoded as a list of instructions
+  """
+  program::Vector{Instruction} = Instruction[]
 
-    """
-    The total number of addresses we need.
-    """
-    memsize = 1
+  """
+  The total number of addresses we need.
+  """
+  memsize = 1
 end
 
 function ematch_compile(p::AbstractPat, pvars, direction::Int)
-    state = EMatchCompilerState(; patvar_to_addr = fill(-1, length(pvars)))
+  state = EMatchCompilerState(; patvar_to_addr = fill(-1, length(pvars)))
 
-    ematch_compile_ground!(p, state, 1)
+  ematch_compile_ground!(p, state, 1)
 
-    state.first_nonground = state.memsize
-    state.memsize += 1
+  state.first_nonground = state.memsize
+  state.memsize += 1
 
-    ematch_compile!(p, state, state.first_nonground)
+  ematch_compile!(p, state, state.first_nonground)
 
-    push!(state.program, Yield(state.patvar_to_addr, direction))
+  push!(state.program, Yield(state.patvar_to_addr, direction))
 
-    pat_constants_checks = check_constant_exprs!(Expr[], p)
+  pat_constants_checks = check_constant_exprs!(Expr[], p)
 
-    quote
-        function $(gensym("ematcher"))(
-            g::$(Metatheory.EGraphs.EGraph),
-            rule_idx::Int,
-            root_id::$(Metatheory.Id),
-            stack::$(Metatheory.OptBuffer){UInt16},
-            ematch_buffer::$(Metatheory.OptBuffer){UInt128},
-        )::Int
-            # If the constants in the pattern are not all present in the e-graph, just return
-            $(pat_constants_checks...)
+  quote
+    function $(gensym("ematcher"))(
+      g::$(Metatheory.EGraphs.EGraph),
+      rule_idx::Int,
+      root_id::$(Metatheory.Id),
+      stack::$(Metatheory.OptBuffer){UInt16},
+      ematch_buffer::$(Metatheory.OptBuffer){UInt128},
+    )::Int
+      # If the constants in the pattern are not all present in the e-graph, just return
+      $(pat_constants_checks...)
 
-            # Initialize σ variables (e-classes memory) and enode iteration indexes
-            $(make_memory(state.memsize, state.first_nonground)...)
-            $((map(state.enode_var_addresses) do addr
-                :($(enode_var(addr)) = 1)
-            end)...)
+      # Initialize σ variables (e-classes memory) and enode iteration indexes
+      $(make_memory(state.memsize, state.first_nonground)...)
+      $((map(state.enode_var_addresses) do addr
+        :($(enode_var(addr)) = 1)
+      end)...)
 
-            n_matches = 0
-            # Backtracking stack
-            stack_idx = 0
+      n_matches = 0
+      # Backtracking stack
+      stack_idx = 0
 
-            # Instruction 0 is used to return when  the backtracking stack is empty.
-            # We start from 1.
-            push!(stack, 0x0000)
-            pc = 0x0001
+      # Instruction 0 is used to return when  the backtracking stack is empty.
+      # We start from 1.
+      push!(stack, 0x0000)
+      pc = 0x0001
 
-            # We goto this label when:
-            # 1) After backtracking, the pc is popped from the stack.
-            # 2) When an instruction succeeds, the pc is incremented.
-            @label compute
-            # Instruction 0 is used to return when  the backtracking stack is empty.
-            pc === 0x0000 && return n_matches
+      # We goto this label when:
+      # 1) After backtracking, the pc is popped from the stack.
+      # 2) When an instruction succeeds, the pc is incremented.
+      @label compute
+      # Instruction 0 is used to return when  the backtracking stack is empty.
+      pc === 0x0000 && return n_matches
 
-            # For each instruction in the program, create an if statement,
-            # Checking if the current value
-            $((map(enumerate(state.program)) do (i, inst)
-                quote
-                    if pc === $(UInt16(i))
-                        $(to_expr(inst))
-                    end
-                end
-            end)...)
+      # For each instruction in the program, create an if statement,
+      # Checking if the current value
+      $((map(enumerate(state.program)) do (i, inst)
+        quote
+          if pc === $(UInt16(i))
+            $(to_expr(inst))
+          end
+        end
+      end)...)
 
-            error("unreachable code!")
+      error("unreachable code!")
 
-            @label backtrack
-            pc = pop!(stack)
+      @label backtrack
+      pc = pop!(stack)
 
-            @goto compute
+      @goto compute
 
-            return -1
-      end
+      return -1
     end
+  end
 end
 
 check_constant_exprs!(buf, p::PatLiteral) = push!(buf, :(has_constant(g, $(last(p.n))) || return 0))
@@ -406,7 +406,7 @@ where root_id is a parameter of the ematching function, defined
 in scope.
 """
 make_memory(n, first_nonground) = map(1:n) do i
-    :($(eclass_var(i)) = $(i == first_nonground ? :root_id : Id(0)))
+  :($(eclass_var(i)) = $(i == first_nonground ? :root_id : Id(0)))
 end
 
 # ==============================================================
@@ -453,7 +453,7 @@ end
 # ==============================================================
 
 """
-    ematch_compile!(p::AbstractPat, state::EMatchCompilerState, addr::Int)
+ematch_compile!(p::AbstractPat, state::EMatchCompilerState, addr::Int)
 
 Emit instructions into `state` that e-match `p` with the e-class that is stored
 in `eclass_var(addr)`.
@@ -461,7 +461,7 @@ in `eclass_var(addr)`.
 function ematch_compile! end
 
 """
-    ematch_compile!(p::PatExpr, state::EMatchCompilerState, addr::Int)
+ematch_compile!(p::PatExpr, state::EMatchCompilerState, addr::Int)
 
 Loop over all the e-nodes in `eclass_var(addr)`, and for each one whose head matches
 `p`, place its arguments in new addresses, and keep going.
@@ -488,7 +488,7 @@ function ematch_compile!(p::PatExpr, state::EMatchCompilerState, addr::Int)
 end
 
 """
-    ematch_compile!(p::PatVar, state::EMatchCompilerState, addr::Int)
+ematch_compile!(p::PatVar, state::EMatchCompilerState, addr::Int)
 
 If we have previously bound this var, check if the eclass id in `eclass_var(addr)`
 is equal to the previously-bound eclass id.
@@ -522,7 +522,7 @@ function ematch_compile!(p::AbstractPat, state::EMatchCompilerState, ::Int)
 end
 
 """
-    ematch_compile!(p::PatLiteral, state::EMatchCompilerState, addr::Int)
+ematch_compile!(p::PatLiteral, state::EMatchCompilerState, addr::Int)
 
 This is just like the `isground` case of `ematch_compile(p::PatExpr,...)`; we
 have already bound all of the constant terms in the pattern to e-classes, so
