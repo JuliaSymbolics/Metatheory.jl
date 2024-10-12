013253e61b804137c04c662ed89193e53cbb21a8
Gabriel Kronberger <gabriel.kronberger@heuristiclab.com>
2024-10-12 12:28:02 +0200

    Add some test assertions for internal datastructures used for egraph rebuilding.

--- test/egraphs/egraphs.jl
+++ test/egraphs/egraphs.jl
@@ -4,10 +4,25 @@ using Test, Metatheory
 @testset "Merging" begin
   testexpr = :((a * 2) / 2)
   testmatch = :(a << 1)
-  g = EGraph(testexpr)
+  g = EGraph()
+  testexpr_id = addexpr!(g, testexpr)
+  t1 = addexpr!(g, :(a * 2)) # get eclass id of a * 2
   t2 = addexpr!(g, testmatch)
-  union!(g, t2, Id(3))
-  @test find(g, t2) == find(g, Id(3))
+  union!(g, t2, t1)
+
+  @testset "Behaviour" begin
+    @test find(g, t2) == find(g, t1)
+  end
+
+  @testset "Internals" begin
+    @test length(g[t1].nodes) == 2 # a << 1, a * 2
+    @test g[t1].parents == [testexpr_id]
+
+    id_1 = addexpr!(g, 1) # get id of constant 1
+    @test g[id_1].parents == [find(g, t1)] # just eclass [a << 1, a * 2]
+    id_a = addexpr!(g, :a)
+    @test g[id_a].parents == [find(g, t1)] # just eclass [a << 1, a * 2]
+  end
 end
 
 # testexpr = :(42a + b * (foo($(Dict(:x => 2)), 42)))
@@ -25,10 +40,20 @@ end
   t2 = addexpr!(g, :c)
 
   union!(g, t2, t1)
-  @test find(g, t2) == find(g, t1)
-  @test find(g, t2) == find(g, t1)
-  rebuild!(g)
-  @test find(g, ec1) == find(g, ec2)
+  @testset "Behaviour" begin
+    @test find(g, t2) == find(g, t1)
+    rebuild!(g)
+    @test find(g, ec1) == find(g, ec2)
+    @test length(g[ec2].nodes) == 1
+  end
+
+  @testset "Internals" begin
+    aid = addexpr!(g, :a) # get id of :a
+    @test g[aid].parents == [find(g, ec1)]
+    @test g[t1].parents == [find(g, ec1)]
+    @test g[ec1].parents == [find(g, testec)]
+    @test length(g[testec].nodes) == 1
+  end
 end
 
 
@@ -53,19 +78,26 @@ end
   t3 = addexpr!(g, apply(3, f, :a))
   t4 = addexpr!(g, apply(7, f, :a))
 
-  # f^m(a) = a = f^n(a) ⟹ f^(gcd(m,n))(a) = a
-  @test find(g, t1) == find(g, a)
-  @test find(g, t2) == find(g, a)
-  @test find(g, t3) == find(g, a)
-  @test find(g, t4) != find(g, a)
+  @testset "Behaviour" begin
+    # f^m(a) = a = f^n(a) ⟹ f^(gcd(m,n))(a) = a
+    @test find(g, t1) == find(g, a)
+    @test find(g, t2) == find(g, a)
+    @test find(g, t3) == find(g, a)
+    @test find(g, t4) != find(g, a)
 
-  # if m or n is prime, f(a) = a
-  t5 = addexpr!(g, apply(11, f, :a))
-  t6 = addexpr!(g, apply(1, f, :a))
-  c5_id = union!(g, t5, a) # a == apply(11,f,a)
+    # if m or n is prime, f(a) = a
+    t5 = addexpr!(g, apply(11, f, :a))
+    t6 = addexpr!(g, apply(1, f, :a))
+    c5_id = union!(g, t5, a) # a == apply(11,f,a)
 
-  rebuild!(g)
+    rebuild!(g)
 
-  @test find(g, t5) == find(g, a)
-  @test find(g, t6) == find(g, a)
+    @test find(g, t5) == find(g, a)
+    @test find(g, t6) == find(g, a)
+  end
+
+  @testset "Internals" begin
+    @test length(g.classes) == 1 # only a single class %id [:a, f(%id)] remains
+    @test g[a].parents = [find(g, a)] # there can be only a single parent
+  end
 end
