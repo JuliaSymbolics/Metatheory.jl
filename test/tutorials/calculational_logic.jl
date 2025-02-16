e30a0fb9f105f6aa032d7e07eafc9729f360667c
a <a@a.a>
2025-02-16 13:27:43 +0100

    fix benchmarks

--- test/tutorials/calculational_logic.jl
+++ test/tutorials/calculational_logic.jl
@@ -22,7 +22,11 @@ include(joinpath(dirname(pathof(Metatheory)), "../examples/calculational_logic_t
 
   freges = :((p ⟹ (q ⟹ r)) ⟹ ((p ⟹ q) ⟹ (p ⟹ r)))   # Frege's theorem
   params = SaturationParams(timeout = 12, eclasslimit = 10000, schedulerparams = (match_limit = 6000, ban_length = 5))
-  @test_broken true == prove(calculational_logic_theory, freges, 2, 10, params)
+  # TODO FIXME After https://github.com/JuliaSymbolics/Metatheory.jl/pull/261/ the order of application of
+  # matches in ematch_buffer has been reversed. There is likely some issue in rebuilding such that the
+  # order of application of rules changes the resulting e-graph, while this should not be the case.
+  # See comments in https://github.com/JuliaSymbolics/Metatheory.jl/pull/261#pullrequestreview-2609050078
+  @test prove(reverse(calculational_logic_theory), freges, 2, 10, params)
 
   @test prove(calculational_logic_theory, :(!(p || q) == (!p && !q)))   # Demorgan's
 end
